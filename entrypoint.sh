d98d98f4083d6f8371275cc777368509ff9ab90c
Axel Köhler <axel.koehler@kiwigrid.com>
2018-12-17 17:57:43 +0100

    Additional fetch if first key recv fails; bump gcloud sdk

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,23 +2,29 @@
 
 if [ -n "${GPG_PUB_KEYS}" ]; then
   for KEY in ${GPG_PUB_KEYS}; do
-    gpg --keyserver hkps.pool.sks-keyservers.net --recv ${KEY};
+    gpg --keyserver hkp://ipv4.pool.sks-keyservers.net --recv ${KEY};
+
+    if [[ $? != 0 ]]; then
+      # Fallback if first fetch failed
+      gpg --keyserver hkp://pgp.mit.edu:80 --recv ${KEY};
+    fi
+
   done
 fi
 
 if [ -n "${REPO_YAML_URL}" ]; then
   REPO_YAML_PATH="/tmp/repo-values.yaml"
-  curl --location --silent --show-error --output $REPO_YAML_PATH $REPO_YAML_URL 
-  if [ -e $REPO_YAML_PATH ]; then  
+  curl --location --silent --show-error --output $REPO_YAML_PATH $REPO_YAML_URL
+  if [ -e $REPO_YAML_PATH ]; then
     size=$(yq r $REPO_YAML_PATH 'sync.repos[*].name' | wc -l )
     index=$(expr $size - 1)
-    for i in $(seq 0 $index); do 
-      helm repo add $(yq r $REPO_YAML_PATH sync.repos[$i].name) $(yq r $REPO_YAML_PATH sync.repos[$i].url) ; 
+    for i in $(seq 0 $index); do
+      helm repo add $(yq r $REPO_YAML_PATH sync.repos[$i].name) $(yq r $REPO_YAML_PATH sync.repos[$i].url) ;
     done
-  else 
-    echo "Download repo-values.yaml failed from URL ${REPO_YAML_URL}." 
+  else
+    echo "Download repo-values.yaml failed from URL ${REPO_YAML_URL}."
   fi
-fi 
+fi
 
 if [ -n "${1}" ]; then
   ${1}
