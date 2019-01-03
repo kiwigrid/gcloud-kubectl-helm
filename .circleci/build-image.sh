446a64d5f85670c6e78454fdb388d47a4867de71
André Bauer <andre.bauer@kiwigrid.com>
2019-01-03 18:51:55 +0100

    changed build command

--- .circleci/build-image.sh
+++ .circleci/build-image.sh
@@ -9,4 +9,4 @@ set -o pipefail
 DOCKER_TAG="$(grep -E '(ENV HELM_VERSION|FROM google/cloud-sdk)' Dockerfile | sed -e 's#ENV\ HELM_VERSION v##' -e 's#FROM\ google/cloud-sdk:##' -e 's#alpine##' | awk '{print}' ORS='')"
 
 echo "Build Docker image with tag ${DOCKER_TAG} for DockerHubs ${DOCKER_REGISTRY}/${GITHUB_USERNAME}/${DOCKER_REPOSITORY} repo"
-docker build --pull --no-cache -t "${DOCKER_REGISTRY}/${GITHUB_USERNAME}/${DOCKER_REPOSITORY}":latest -t "${DOCKER_REGISTRY}/${GITHUB_USERNAME}/${DOCKER_REPOSITORY}:${DOCKER_TAG}" .
+docker build --pull --no-cache -t "${DOCKER_REGISTRY}/${GITHUB_USERNAME}/${DOCKER_REPOSITORY}:latest" -t "${DOCKER_REGISTRY}/${GITHUB_USERNAME}/${DOCKER_REPOSITORY}:${DOCKER_TAG}" .
