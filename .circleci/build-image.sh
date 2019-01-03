e7a6df00a63da7bf53dd6051eda13ba36fb89839
André Bauer <andre.bauer@kiwigrid.com>
2019-01-03 17:59:20 +0100

    added circleci

---
+++ .circleci/build-image.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# install charts in kubernetes kind
+#
+
+set -o errexit
+set -o pipefail
+
+DOCKER_TAG="$(grep -E '(ENV HELM_VERSION|FROM google/cloud-sdk)' Dockerfile | sed -e 's#ENV\ HELM_VERSION v##' -e 's#FROM\ google/cloud-sdk:##' -e 's#alpine##' | awk '{print}' ORS='')"
+
+echo "Build Docker image with tag ${DOCKER_TAG} for DockerHubs ${DOCKER_REGISTRY}/${GITHUB_USERNAME}/${DOCKER_REPOSITORY} repo"
+docker build --pull --no-cache -t "${DOCKER_REGISTRY}/${GITHUB_USERNAME}/${DOCKER_REPOSITORY}":latest -t "${DOCKER_REGISTRY}/${GITHUB_USERNAME}/${DOCKER_REPOSITORY}:${DOCKER_TAG}" .
---
+++ .circleci/push-image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# deploy to dockerhub
+#
+
+set -o errexit
+set -o pipefail
+
+DOCKER_TAG="$(grep -E '(ENV HELM_VERSION|FROM google/cloud-sdk)' Dockerfile | sed -e 's#ENV\ HELM_VERSION v##' -e 's#FROM\ google/cloud-sdk:##' -e 's#alpine##' | awk '{print}' ORS='')"
+
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+docker push "${DOCKER_REGISTRY}/${GITHUB_USERNAME}/${DOCKER_REPOSITORY}:${DOCKER_TAG}"
+docker push "${DOCKER_REGISTRY}/${GITHUB_USERNAME}/${DOCKER_REPOSITORY}:latest"
+
+bla=1
