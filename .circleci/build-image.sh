0278492f662dc1cced8664d37e7de59241ae3392
André Bauer <andre.bauer@kiwigrid.com>
2019-01-04 13:55:27 +0100

    removed env

--- .circleci/build-image.sh
+++ .circleci/build-image.sh
@@ -6,8 +6,6 @@
 set -o errexit
 set -o pipefail
 
-env | sort
-
 DOCKER_TAG="$(grep -E '(ENV HELM_VERSION|FROM google/cloud-sdk)' Dockerfile | sed -e 's#ENV\ HELM_VERSION v##' -e 's#FROM\ google/cloud-sdk:##' -e 's#alpine##' | awk '{print}' ORS='')"
 
 echo "Build Docker image with tag ${DOCKER_TAG} for DockerHubs ${DOCKER_REGISTRY}/${GITHUB_USERNAME}/${DOCKER_REPOSITORY} repo"
