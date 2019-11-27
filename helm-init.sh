ba0b75197fff7a47bc240c952dbdcb94e250c214
André Bauer <andre.bauer@kiwigrid.com>
2019-11-27 13:05:17 +0100

    added missing helm-init.sh file

---
+++ helm-init.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+if helm version --client | grep -q 'SemVer:"v2';then 
+    helm init --client-only
+else 
+    helm repo add stable https://kubernetes-charts.storage.googleapis.com
+fi
+
+helm plugin install https://github.com/futuresimple/helm-secrets.git
