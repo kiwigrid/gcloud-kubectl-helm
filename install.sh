4b0d85e0d3dcf1ea59ef92b9abdefdeef8129086
André Bauer <monotek@users.noreply.github.com>
2019-12-08 00:57:35 +0100

    added comments & some cleanup

--- install.sh
+++ install.sh
@@ -3,28 +3,33 @@
 set -e
 set -x
 
+# add gkh user
 adduser -S gkh gkh
 
+# install apk packages
 apk update
 apk add ca-certificates gnupg mysql-client openssl
 rm -rf /var/cache/apk/*
 
+# install cloud_sql_proxy & kubectl
 gcloud components install -q beta cloud_sql_proxy kubectl
 
+# install helm
 curl --silent --show-error --fail --location --output get_helm.sh https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get
 chmod 700 get_helm.sh
-
 ./get_helm.sh --version "${HELM_VERSION}"
+rm get_helm.sh
 
+# install sops
 curl --silent --show-error --fail --location --output /usr/local/bin/sops https://github.com/mozilla/sops/releases/download/"${SOPS_VERSION}"/sops-"${SOPS_VERSION}".linux
 chmod 755 /usr/local/bin/sops
 
+# install yq
 curl --silent --show-error --fail --location --output /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/"${YQ_BIN_VERSION}"/yq_linux_amd64
 chmod 755 /usr/local/bin/yq
 
+# set permissions
 mkdir -p /data
-chown gkh /data
-chown gkh /entrypoint.sh
-chmod +x /entrypoint.sh
-chown gkh /data/commands.sh
-chmod +x /data/commands.sh
+chown gkh /data /entrypoint.sh /data/commands.sh
+chmod +x /entrypoint.sh /data/commands.sh
+
