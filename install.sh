9e1bc7039bd562c264273eb4b60469ed3449d950
André Bauer <monotek@users.noreply.github.com>
2019-12-08 01:35:38 +0100

    added --no-cache to apk command

--- install.sh
+++ install.sh
@@ -8,8 +8,7 @@ adduser -S gkh gkh
 
 # install apk packages
 apk update
-apk add ca-certificates gnupg mysql-client openssl
-rm -rf /var/cache/apk/*
+apk --no-cache add ca-certificates gnupg mysql-client openssl
 
 # install cloud_sql_proxy & kubectl
 gcloud components install -q beta cloud_sql_proxy kubectl
