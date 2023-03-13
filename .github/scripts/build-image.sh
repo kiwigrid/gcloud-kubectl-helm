20cbfd286e3c7d0191dee9e68223647e989aab96
Jens Puruckherr <jens.puruckherr@kiwigrid.com>
2023-03-13 12:30:21 +0100

    adjust shell variables; adapt workflow

--- .github/scripts/build-image.sh
+++ .github/scripts/build-image.sh
@@ -11,26 +11,28 @@ GCLOUD_SDK_VERSION="$(grep google/cloud-sdk Dockerfile | sed -e 's#FROM\ google/
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 
 for HELM in ${HELM_VERSIONS}; do
-  DOCKER_TAG="${HELM}-${GCLOUD_SDK_VERSION}-${CIRCLE_BUILD_NUM}"
+  DOCKER_TAG="${HELM}-${GCLOUD_SDK_VERSION}-${GITHUB_RUN_NUMBER}"
 
   sed -i "s/get_helm.sh --version.*/get_helm.sh --version v${HELM}/" "${REPO_ROOT}"/install.sh
 
   # build image
-  echo "Build Docker image with tag ${DOCKER_TAG} for DockerHubs ${DOCKER_REGISTRY}/${GITHUB_USERNAME}/${DOCKER_REPOSITORY} repo"
+  echo "Build Docker image with tag ${DOCKER_TAG} for DockerHubs ${DOCKER_REGISTRY}/${GITHUB_REPOSITORY_OWNER}/${DOCKER_REPOSITORY} repo"
 
   if ! echo "${HELM}" | grep -q '^2.*'; then
-    docker build --pull --no-cache -t "${DOCKER_REGISTRY}/${GITHUB_USERNAME}/${DOCKER_REPOSITORY}:latest" -t "${DOCKER_REGISTRY}/${GITHUB_USERNAME}/${DOCKER_REPOSITORY}:${DOCKER_TAG}" .
+    docker build --pull --no-cache -t "${DOCKER_REGISTRY}/${GITHUB_REPOSITORY_OWNER}/${DOCKER_REPOSITORY}:latest" -t "${DOCKER_REGISTRY}/${GITHUB_REPOSITORY_OWNER}/${DOCKER_REPOSITORY}:${DOCKER_TAG}" .
   else
-    docker build --pull --no-cache -t "${DOCKER_REGISTRY}/${GITHUB_USERNAME}/${DOCKER_REPOSITORY}:${DOCKER_TAG}" .
+    docker build --pull --no-cache -t "${DOCKER_REGISTRY}/${GITHUB_REPOSITORY_OWNER}/${DOCKER_REPOSITORY}:${DOCKER_TAG}" .
   fi
 
-  if [ "${CIRCLECI}" == 'true' ] && [ -z "${CIRCLE_PULL_REQUEST}" ]; then
+  #if [ "${CIRCLE_CI}" == 'true' ] && [ -z "${CIRCLE_PULL_REQUEST}" ]; then
+  if [ "${GITHUB_ACTIONS}" == 'true' ]; then
     # push image to dockerhub
     echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-    docker push "${DOCKER_REGISTRY}/${GITHUB_USERNAME}/${DOCKER_REPOSITORY}:${DOCKER_TAG}"
+    #  https://hub.docker.com/r/kiwigrid/gcloud-kubectl-helm
+    docker push "${DOCKER_REGISTRY}/${GITHUB_REPOSITORY_OWNER}/${DOCKER_REPOSITORY}:${DOCKER_TAG}"
 
     if ! echo "${HELM}" | grep -q '^2.*'; then
-      docker push "${DOCKER_REGISTRY}/${GITHUB_USERNAME}/${DOCKER_REPOSITORY}:latest"
+      docker push "${DOCKER_REGISTRY}/${GITHUB_REPOSITORY_OWNER}/${DOCKER_REPOSITORY}:latest"
     fi
   else
     echo "skipped push as pull requests are not build..."
